4ba5b4765c209f544fb3f4b2ce224fdcba2a78d8
eno-conan <eno49study@gmail.com>
2022-12-07 21:10:34 +0900

    Create Link to inputRecordPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -13,12 +14,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Let's Visit <Link href="/inputRecordPage">Input Record Page</Link>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          Get started
         </p>
 
         <div className={styles.grid}>
