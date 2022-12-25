ba7cf0acb7cea5077bf0b619266b9c89fa82064d
eno-conan <eno49study@gmail.com>
2022-12-25 14:30:19 +0900

    divide test files related multi-page-routing

--- playwright/routing-multi-case1.test.ts
+++ playwright/routing-multi-case1.test.ts
@@ -1,21 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test('go to /targetRecordPage/XXX and to Top Page', async ({ page }) => {
-  await page.goto('/searchRecordPage');
-  const id = await page.getByTestId('account-delete-confirm');
-  await page.locator("text=Sleep防止");
-  await page.click("text=Sleep防止");
-  await page.waitForTimeout(500);
-  await page.click("text=詳細確認");
-  await page.waitForTimeout(3000);
-  await page.waitForSelector(`text=Sleep防止`);
-  const baseUrl = process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000';
-  await expect(page).toHaveURL(new RegExp(`${baseUrl}/targetRecordPage/[0-9]`));
-  const a = await page.click("text=Top");
-  await page.waitForTimeout(1000);
-  await expect(page).toHaveURL(new RegExp(`${baseUrl}`));
-});
-
 test('go to /targetRecordPage/XXX and to SearchRecord Page', async ({ page }) => {
   await page.goto('/searchRecordPage');
   const id = await page.getByTestId('account-delete-confirm');
